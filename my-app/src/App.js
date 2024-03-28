072b205c816c117dc50b081b90e9ff0338f6a67b
Kat <91990952+Rebelkat18@users.noreply.github.com>
2024-03-28 11:25:54 -0400

    React App Workshop

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Computer Club Workshop</h1>
+      <p> Steve's Doggo </p>
+      <img src="doggo.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
